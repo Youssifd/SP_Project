54cc3e6e2eae84853a4930a715703248eba4f1c9
Youssif El-Sayed <Yousefd4704@gmail.com>
2024-03-30 18:55:21 +0200

    functions

--- Project/project's code .cpp
+++ Project/project's code .cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define NumberOfHospital 5
 #define NumberOfReservation 10
 #define NumberOfUsers 8
-#define NumberOfSpecialties 7
-#define NumberOfClinics 14
+#define NumberOfSpecialties 11
+#define NumberOfClinics 19
 
 struct Hospitals {
 	 int HospitalID;
@@ -45,13 +45,6 @@ void DefinitonOfVariable(Hospitals hospital[]);
 void main() {
 	 Hospitals hospital[NumberOfHospital];
 	 DefinitonOfVariable(hospital);
-	 cout << "-------------------Before sorting by name---------------------\n";
-	 for (int i = 0; i < NumberOfHospital; i++)
-		  cout << hospital[i].HospitalName << "\n";
-	 cout << "-------------------after sorting by name---------------------\n";
-	 SortHospitalByName(hospital);
-	 for (int i = 0; i < NumberOfHospital; i++)
-		  cout << hospital[i].HospitalName << "\n";
 	 cout << "-------------------Before sorting by Rate---------------------\n";
 	 for (int i = 0; i < NumberOfHospital; i++)
 		  cout << hospital[i].HospitalName<<"--> "<< hospital[i].HospitalRate << "\n";
@@ -59,6 +52,15 @@ void main() {
 	 SortHospitalByRating(hospital);
 	 for (int i = 0; i < NumberOfHospital; i++)
 		  cout << hospital[i].HospitalName << "--> " << hospital[i].HospitalRate << "\n";
+	 cout << "-------------------Before sorting by name---------------------\n";
+	 for (int i = 0; i < NumberOfHospital; i++)
+		  cout << hospital[i].HospitalName << "\n";
+	 cout << "-------------------after sorting by name---------------------\n";
+	 SortHospitalByName(hospital);
+	 for (int i = 0; i < NumberOfHospital; i++)
+		  cout << hospital[i].HospitalName << "\n";
+ cout << "-------------------Print hospital Data---------------------\n";
+	 PrintHospitalData(hospital);
 }
 void SortHospitalByName(Hospitals hospital[]) {
 	 for (int i = 0; i < NumberOfHospital - 1; ++i) {
@@ -92,16 +94,63 @@ void SortByBedsPrice(Hospitals hospital[]) {
 		  }
 	 }
 }
+void PrintHospitalData(Hospitals hospital[]) {
+	 for (int i = 0; i < NumberOfHospital; i++) {
+		  cout << "Hospital name: " << hospital[i].HospitalName << "\nHospital ID: " << hospital[i].HospitalID << "\nNunmber of beds available: " << hospital[i].PatientReservationRooms << "\nBeds price per night: " << hospital[i].BedsPrice << "\nReservation price for check-up: " << hospital[i].ReservationPrice << "\nHospiral Rate: " << hospital[i].HospitalRate << endl;
+		  cout << "-----------------------------------------------\n";
+		  cout << "Hospital Specialties:\n";
+		  for (int j = 0; j < NumberOfSpecialties; j++)
+		  {
+			   cout << j + 1 << "-" << hospital[i].HospitalSpecialties[j] << ". ";
+			   if ((j - 1) % 2 == 0)
+					cout << "\n";
+		  }
+		  cout << "\n-----------------------------------------------\n";
+		  cout << "Hospital Clinics:\n";
+		  for (int j = 0; j < NumberOfClinics; j++)
+		  {
+			   cout << j + 1 << "-" << hospital[i].HospitalClinics[j] << ". ";
+			   if ((j - 4) % 5 == 0)
+					cout << "\n";
+		  }
+		  cout << "\n============================================\n";
+	 }
+}
 void DefinitonOfVariable(Hospitals hospital[]) {
 	 ifstream HospitalInfo("Data/Hospitalinfo.txt", ios::app);
-	 for (int i = 0; i < NumberOfHospital; i++)
-		  getline(HospitalInfo, hospital[i].HospitalName);
+	
 	 for (int i = 0; i < NumberOfHospital; i++) {
+		  getline(HospitalInfo, hospital[i].HospitalName);
 		  HospitalInfo >> hospital[i].HospitalID >> hospital[i].PatientReservationRooms >> hospital[i].ReservationPrice >> hospital[i].BedsPrice >> hospital[i].HospitalRate;
 		  for(int j=0;j< NumberOfSpecialties;j++)
 			   HospitalInfo >> hospital[i].HospitalSpecialties[j];
 		  for (int j = 0; j < NumberOfClinics; j++)
 			   HospitalInfo >> hospital[i].HospitalClinics[j];
+		  HospitalInfo.ignore();
+		  HospitalInfo.ignore();
 	 }
 	 HospitalInfo.close();
 }
+/*
+El-Demerdash
+1931101 664 80 600 3.5
+Dentist InternalMedicine Neurosurgery Ophthalmology Otorhinolaryngology Pediatrics Oncology Emergency Anesthesiology X-Ray ObesitySurgeries
+Otorhinolaryngology Ophthalmia Dentist Nephrology Cardiology Pulmonology Neurology BloodVessels Phoniatrics ObstetricsAndGynecology Dermatology Orthopedics Endocrinology Urology Psychiatry NaturalTherapy Nutrition CosmeticMedicine InternalMedicine
+Ain Shams Specialized
+1984102 700 250 1200 3.8
+Dentist InternalMedicine Neurosurgery Ophthalmology Otorhinolaryngology Pediatrics Oncology Emergency Anesthesiology X-Ray ObesitySurgeries
+Otorhinolaryngology Ophthalmia Dentist Nephrology Cardiology Pulmonology Neurology BloodVessels Phoniatrics ObstetricsAndGynecology Dermatology Orthopedics Endocrinology Urology Psychiatry NaturalTherapy Nutrition CosmeticMedicine InternalMedicine
+Al-Qasr Al-Aini
+1827103 536 80 600 4.0
+Dentist InternalMedicine Neurosurgery Ophthalmology Otorhinolaryngology Pediatrics Oncology Emergency Anesthesiology X-Ray ObesitySurgeries
+Otorhinolaryngology Ophthalmia Dentist Nephrology Cardiology Pulmonology Neurology BloodVessels Phoniatrics ObstetricsAndGynecology Dermatology Orthopedics Endocrinology Urology Psychiatry NaturalTherapy Nutrition CosmeticMedicine InternalMedicine
+Sayed Galal
+1945104 1500 100 400 3.6
+Dentist InternalMedicine Neurosurgery Ophthalmology Otorhinolaryngology Pediatrics Oncology Emergency Anesthesiology X-Ray ObesitySurgeries
+Otorhinolaryngology Ophthalmia Dentist Nephrology Cardiology Pulmonology Neurology BloodVessels Phoniatrics ObstetricsAndGynecology Dermatology Orthopedics Endocrinology Urology Psychiatry NaturalTherapy Nutrition CosmeticMedicine InternalMedicine
+Dar Al-Fouad
+1999105 1050 300 1200 3.7
+Dentist InternalMedicine Neurosurgery Ophthalmology Otorhinolaryngology Pediatrics Oncology Emergency Anesthesiology X-Ray ObesitySurgeries
+Otorhinolaryngology Ophthalmia Dentist Nephrology Cardiology Pulmonology Neurology BloodVessels Phoniatrics ObstetricsAndGynecology Dermatology Orthopedics Endocrinology Urology Psychiatry NaturalTherapy Nutrition CosmeticMedicine InternalMedicine
+
+*/
