bcdf8658479bfa5f2fe2f48da148c5ec13a821f2
Youssif El-Sayed <Yousefd4704@gmail.com>
2024-04-14 23:24:42 +0200

    Update project's code .cpp

--- Project/project's code .cpp
+++ Project/project's code .cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<conio.h>
 using namespace std;
 #define NumberOfHospital 5
-#define NumberOfReservation 10
-#define NumberOfUsers 15
-#define NumberOfSpecialties 11
-#define NumberOfClinics 19
+#define NumberOfReservation 20
+#define NumberOfUsers 20
+#define NumberOfSpecialties 20
+#define NumberOfClinics 25
 
 struct Hospitals {
 	 int HospitalID;
 	 int PatientReservationRooms;//BedsAvailable
-	 float ReservationPrice;//for check-up
+	 float ReservationPrice; //for check-up
 	 float BedsPrice; //for surgery
 	 float HospitalRate;
+	 int specialtiesCount = 11; // to stop print garbage at other hos.(screen shot)
+	 int clinicsCount = 19;
 	 string HospitalName;
 	 string HospitalSpecialties[NumberOfSpecialties];
 	 string HospitalClinics[NumberOfClinics];
@@ -21,8 +24,8 @@ struct Hospitals {
 struct Reservations {
 	 int ReservtionID;
 	 Hospitals hospital;
-	 string ReservationType[2] = { "check-up","surgery" };
-	 string ReservationDay[7] = { "Saturday","Sunday","Monday","Tuesday","Wednesday"," Thursday","Friday" };
+	 string ReservationType; // { "check-up","surgery" }
+	 string ReservationDay; 
 };
 struct Users {
 	 int id;
@@ -34,37 +37,59 @@ struct Users {
 	 string password;
 };
 
-void PrintHospitalData(Hospitals hospital[]);
+void DefinitonOfVariable(Hospitals hospital[],int& hospitalCount);
+void DefinitonOfVariable(Users user[], int& userCount);
+void PrintHospitalData(Hospitals hospital[], int hospitalCount);
 void PrintPatientData(Users user[], int userCount);
-void SortHospitalByName(Hospitals hospital[]);
-void SortHospitalByRating(Hospitals hospital[]);
-void SortByBedsAvailable(Hospitals hospital[]);
-void SortByBedsPrice(Hospitals hospital[]);
-void DefinitonOfVariable(Hospitals hospital[]);
-void DefinitonOfVariable(Users user[],int& userCount );
+void SortHospitalByName(Hospitals hospital[], int hospitalCount);
+void SortHospitalByRating(Hospitals hospital[], int hospitalCount);
+void SortByBedsAvailable(Hospitals hospital[], int hospitalCount);
+void SortByBedsPrice(Hospitals hospital[], int hospitalCount);
 bool loginAsAdmin(Users user[], int userCount);
+void print(Hospitals hos[], int hospitalCount);//test name
 void main() {
 	 Hospitals hospital[NumberOfHospital];
 	 Users user[NumberOfUsers];
-	 int userCount = 0;
-	 DefinitonOfVariable(hospital);
+	 int userCount = 0,hospitalCount=0;
+	 DefinitonOfVariable(hospital,hospitalCount);
 	 DefinitonOfVariable(user,userCount);
-	// PrintPatientData(user, userCount);
-
-	 if (loginAsAdmin(user, userCount)) {
-		  cout << "\nLogin successful!" << endl;
-	 }
-	 else {
-		  cout << "\nLogin failed. Invalid username or password." << endl;
-	 }
-
+	 	 
 }
-void PrintHospitalData(Hospitals hospital[]) {
-	 for (int i = 0; i < NumberOfHospital; i++) {
+void DefinitonOfVariable(Hospitals hospital[],int& hospitalCount) {
+	 ifstream HospitalInfo("Data/Hospitalinfo.txt", ios::app);
+	
+	 for (int i = 0; !HospitalInfo.eof(); i++) {
+		  getline(HospitalInfo, hospital[i].HospitalName);
+		  HospitalInfo >> hospital[i].HospitalID >> hospital[i].PatientReservationRooms >> hospital[i].ReservationPrice >> hospital[i].BedsPrice >> hospital[i].HospitalRate;
+		  for(int j=0;j< hospital[i].specialtiesCount;j++)
+			   HospitalInfo >> hospital[i].HospitalSpecialties[j];
+		  for (int j = 0; j < hospital[i].clinicsCount; j++)
+			   HospitalInfo >> hospital[i].HospitalClinics[j];
+		  HospitalInfo.ignore();
+		  HospitalInfo.ignore();
+		  hospitalCount++;
+	 }
+	 HospitalInfo.close();
+}
+void DefinitonOfVariable(Users user[],int& userCount ) {
+	 ifstream UserInfo("Data/UserInfo.txt", ios::app);
+	 for (int i = 0; !UserInfo.eof(); i++) {
+		  UserInfo >> user[i].id >> user[i].username >> user[i].age >> user[i].email >> user[i].password;
+		  getline(UserInfo,user[i].name);
+		// UserInfo.ignore();
+		  if (user[i].email.find("@Hadmin.com") != string::npos)
+			   user[i].userType = "Admin";
+		
+		  userCount++;
+	 }
+	 UserInfo.close();
+}
+void PrintHospitalData(Hospitals hospital[],int hospitalCount) {
+	 for (int i = 0; i < hospitalCount; i++) {
 		  cout << "Hospital name: " << hospital[i].HospitalName << "\nHospital ID: " << hospital[i].HospitalID << "\nNunmber of beds available: " << hospital[i].PatientReservationRooms << "\nBeds price per night: " << hospital[i].BedsPrice << "\nReservation price for check-up: " << hospital[i].ReservationPrice << "\nHospiral Rate: " << hospital[i].HospitalRate << endl;
 		  cout << "-----------------------------------------------\n";
 		  cout << "Hospital Specialties:\n";
-		  for (int j = 0; j < NumberOfSpecialties; j++)
+		  for (int j = 0; j < hospital[i].specialtiesCount; j++)
 		  {
 			   cout << j + 1 << "-" << hospital[i].HospitalSpecialties[j] << ". ";
 			   if ((j - 1) % 2 == 0)
@@ -72,7 +97,7 @@ void PrintHospitalData(Hospitals hospital[]) {
 		  }
 		  cout << "\n-----------------------------------------------\n";
 		  cout << "Hospital Clinics:\n";
-		  for (int j = 0; j < NumberOfClinics; j++)
+		  for (int j = 0; j < hospital[i].clinicsCount; j++)
 		  {
 			   cout << j + 1 << "-" << hospital[i].HospitalClinics[j] << ". ";
 			   if ((j - 4) % 5 == 0)
@@ -95,65 +120,37 @@ void PrintPatientData(Users user[],int userCount) {
 	 }
 
 }
-void SortHospitalByName(Hospitals hospital[]) {
-	 for (int i = 0; i < NumberOfHospital - 1; ++i) {
-		  for (int j = i + 1; j < NumberOfHospital; j++) {
+void SortHospitalByName(Hospitals hospital[],int hospitalCount) {
+	 for (int i = 0; i < hospitalCount - 1; ++i) {
+		  for (int j = i + 1; j < hospitalCount; j++) {
 			   if (hospital[i].HospitalName > hospital[j].HospitalName)
 					swap(hospital[i], hospital[j]);
 		  }
 	 }
 }
-void SortHospitalByRating(Hospitals hospital[]) {
-	 for (int i = 0; i < NumberOfHospital - 1; ++i) {
-		  for (int j = i + 1; j < NumberOfHospital; j++) {
+void SortHospitalByRating(Hospitals hospital[], int hospitalCount) {
+	 for (int i = 0; i < hospitalCount - 1; ++i) {
+		  for (int j = i + 1; j < hospitalCount; j++) {
 			   if (hospital[i].HospitalRate > hospital[j].HospitalRate)
 					swap(hospital[i], hospital[j]);
 		  }
 	 }
 }
-void SortByBedsAvailable(Hospitals hospital[]) {
-	 for (int i = 0; i < NumberOfHospital - 1; ++i) {
-		  for (int j = i + 1; j < NumberOfHospital; j++) {
+void SortByBedsAvailable(Hospitals hospital[], int hospitalCount) {
+	 for (int i = 0; i < hospitalCount - 1; ++i) {
+		  for (int j = i + 1; j < hospitalCount; j++) {
 			   if (hospital[i].PatientReservationRooms > hospital[j].PatientReservationRooms)
 					swap(hospital[i], hospital[j]);
 		  }
 	 }
 }
-void SortByBedsPrice(Hospitals hospital[]) {
-	 for (int i = 0; i < NumberOfHospital - 1; ++i) {
-		  for (int j = i + 1; j < NumberOfHospital; j++) {
+void SortByBedsPrice(Hospitals hospital[], int hospitalCount) {
+	 for (int i = 0; i < hospitalCount - 1; ++i) {
+		  for (int j = i + 1; j < hospitalCount; j++) {
 			   if (hospital[i].BedsPrice > hospital[j].BedsPrice)
 					swap(hospital[i], hospital[j]);
 		  }
 	 }
-}
-void DefinitonOfVariable(Hospitals hospital[]) {
-	 ifstream HospitalInfo("Data/Hospitalinfo.txt", ios::app);
-	
-	 for (int i = 0; !HospitalInfo.eof(); i++) {
-		  getline(HospitalInfo, hospital[i].HospitalName);
-		  HospitalInfo >> hospital[i].HospitalID >> hospital[i].PatientReservationRooms >> hospital[i].ReservationPrice >> hospital[i].BedsPrice >> hospital[i].HospitalRate;
-		  for(int j=0;j< NumberOfSpecialties;j++)
-			   HospitalInfo >> hospital[i].HospitalSpecialties[j];
-		  for (int j = 0; j < NumberOfClinics; j++)
-			   HospitalInfo >> hospital[i].HospitalClinics[j];
-		  HospitalInfo.ignore();
-		  HospitalInfo.ignore();
-	 }
-	 HospitalInfo.close();
-}
-void DefinitonOfVariable(Users user[],int& userCount ) {
-	 ifstream UserInfo("Data/UserInfo.txt", ios::app);
-	 for (int i = 0; !UserInfo.eof(); i++) {
-		  UserInfo >> user[i].id >> user[i].username >> user[i].age >> user[i].email >> user[i].password;
-		  getline(UserInfo,user[i].name);
-		// UserInfo.ignore();
-		  if (user[i].email.find("@Hadmin.com") != string::npos)
-			   user[i].userType = "Admin";
-		
-		  userCount++;
-	 }
-	 UserInfo.close();
 }
 bool loginAsAdmin(Users user[], int userCount) {
 	 bool loggedIn = false;
@@ -185,4 +182,10 @@ bool loginAsAdmin(Users user[], int userCount) {
 	 } while (count < 5);
 
 	 return loggedIn;
+}
+void print(Hospitals hos[], int hospitalCount) {
+	 for (int i = 0; i < hospitalCount; i++)
+	 {
+		  cout << hos[i].HospitalName << "\n======================\n";
+	 }
 }
