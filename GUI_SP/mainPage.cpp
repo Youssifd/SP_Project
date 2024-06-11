d2e03ed2dab5d1c063844a19cb776b67bfb7ffaf
Youssif El-Sayed <Yousefd4704@gmail.com>
2024-06-12 01:42:30 +0300

    add gui project

---
+++ GUI_SP/mainPage.cpp
@@ -0,0 +1,13 @@
+#include "mainPage.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main()
+{
+	 f.DefinitonOfVariable(hospital);
+	 f.DefinitonOfVariable(user, hospital);
+	 Application::EnableVisualStyles();
+	 Application::SetCompatibleTextRenderingDefault(false);
+	 GUISP::mainPage form;
+	 Application::Run(% form);
+}
